d3893a9693e780afa5cba6e33b79f5843c6fa20f
Gedson Marcelino <gedson.marcelino@gmail.com>
2023-05-07 11:16:27 -0400

    feat: added font weight

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -20,6 +20,17 @@ export default {
   italic: "font-style: italic",
   "not-italic": "font-style: normal",
 
+  // Font Weight
+  "font-thin": "font-weight: 100",
+  "font-extralight": "font-weight: 200",
+  "font-light": "font-weight: 300",
+  "font-normal": "font-weight: 400",
+  "font-medium": "font-weight: 500",
+  "font-semibold": "font-weight: 600",
+  "font-bold": "font-weight: 700",
+  "font-extrabold": "font-weight: 800",
+  "font-black": "font-weight: 900",
+
   // Text Align
   "text-left": "text-align: left",
   "text-center": "text-align: center",
